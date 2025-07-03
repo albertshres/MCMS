3b6f704f415b4b63bc814e322230ab59c73116ec
Albert <albertshres2059@gmail.com>
2025-07-03 09:04:08 +0545

    test

--- login/index.php
+++ login/index.php
@@ -4,7 +4,7 @@ include('connect/connection.php');
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-if(isset($_POST["login"])){
+if (isset($_POST["login"])) {
     $email = mysqli_real_escape_string($connect, trim($_POST['email']));
     $password = trim($_POST['password']);
 
@@ -20,11 +20,11 @@ if(isset($_POST["login"])){
     $sql = mysqli_query($connect, "SELECT * FROM login WHERE email = '$email'");
     $count = mysqli_num_rows($sql);
 
-    if($count > 0){
+    if ($count > 0) {
         $fetch = mysqli_fetch_assoc($sql);
         $hashpassword = $fetch["password"];
 
-        if($fetch["status"] == 0){
+        if ($fetch["status"] == 0) {
             // Generate a new OTP and send it to the user
             $otp = rand(100000, 999999);
             $_SESSION['otp'] = $otp;
@@ -37,7 +37,7 @@ if(isset($_POST["login"])){
                 $mail->isSMTP();
                 $mail->Host = 'smtp.gmail.com';
                 $mail->SMTPAuth = true;
-             
+
                 $mail->SMTPSecure = 'tls';
                 $mail->Port = 587;
 
@@ -56,7 +56,7 @@ if(isset($_POST["login"])){
             } catch (Exception $e) {
                 echo "<script>alert('Failed to send OTP. Mailer Error: {$mail->ErrorInfo}');</script>";
             }
-        } else if(password_verify($password, $hashpassword)){
+        } else if (password_verify($password, $hashpassword)) {
             $_SESSION['loggedin'] = true;
             $_SESSION['session_email'] = $email;
             $_SESSION['session_password'] = $password;
@@ -83,6 +83,7 @@ if(isset($_POST["login"])){
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -101,96 +102,103 @@ if(isset($_POST["login"])){
 
     <title>Login Form</title>
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
-    <div class="container">
-        <a class="navbar-brand" href="#">Login Form</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+        <div class="container">
+            <a class="navbar-brand" href="#">Login Form</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php" style="font-weight:bold; color:black; text-decoration:underline">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="register.php">Register</a>
-                </li>
-            </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php"
+                            style="font-weight:bold; color:black; text-decoration:underline">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                    </li>
+                </ul>
 
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
-<main class="login-form">
-    <div class="cotainer">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Login</div>
-                    <div class="card-body">
-                        <form action="#" method="POST" name="login">
-                            <div class="form-group row">
-                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                <div class="col-md-6">
-                                    <input type="text" id="email_address" class="form-control" name="email" required autofocus>
+    <main class="login-form">
+        <div class="cotainer">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Login</div>
+                        <div class="card-body">
+                            <form action="#" method="POST" name="login">
+                                <div class="form-group row">
+                                    <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail
+                                        Address</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="email_address" class="form-control" name="email" required
+                                            autofocus>
+                                    </div>
                                 </div>
-                            </div>
-<!--                        <div class="form-group row">
+                                <!--                        <div class="form-group row">
     <label for="registration_number" class="col-md-4 col-form-label text-md-right">Registration Number</label>
     <div class="col-md-6">
         <input type="text" id="registration_number" class="form-control" name="registration_number" required>
     </div>
 </div>-->
 
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-                                <div class="col-md-6">
-                                    <input type="password" id="password" class="form-control" name="password" required>
-                                   <br> <i class="bi bi-eye-slash" id="togglePassword"></i></br>
-                                </div>
-                            </div>
-
-        
-
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="checkbox">
-                                        <label>
-                         
-                                            <input type="checkbox" name="remember"> Remember Me 
-                                        </label>
+                                <div class="form-group row">
+                                    <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                                    <div class="col-md-6">
+                                        <input type="password" id="password" class="form-control" name="password"
+                                            required>
+                                        <br> <i class="bi bi-eye-slash" id="togglePassword"></i></br>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="col-md-6 offset-md-4">
-                                <input type="submit" value="Login" name="login">
-                                <a href="recover_psw.php" class="btn btn-link">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
+
+
+                                <div class="form-group row">
+                                    <div class="col-md-6 offset-md-4">
+                                        <div class="checkbox">
+                                            <label>
+
+                                                <input type="checkbox" name="remember"> Remember Me
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 offset-md-4">
+                                    <input type="submit" value="Login" name="login">
+                                    <a href="recover_psw.php" class="btn btn-link">
+                                        Forgot Your Password?
+                                    </a>
+                                </div>
+                        </div>
+                        </form>
                     </div>
-                    </form>
                 </div>
             </div>
         </div>
-    </div>
-    </div>
+        </div>
 
-</main>
+    </main>
 </body>
+
 </html>
 <script>
     const toggle = document.getElementById('togglePassword');
     const password = document.getElementById('password');
 
-    toggle.addEventListener('click', function(){
-        if(password.type === "password"){
+    toggle.addEventListener('click', function () {
+        if (password.type === "password") {
             password.type = 'text';
-        }else{
+        } else {
             password.type = 'password';
         }
         this.classList.toggle('bi-eye');
